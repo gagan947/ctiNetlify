8c824cd42951aa8781ea6acb4bf70a122e6ce53e
govindkumawat123 <govind.ctinfotech@gmail.com>
2025-08-07 17:53:01 +0530

    Design Home page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -35,6 +35,10 @@ $(document).ready(function () {
   $(".ct_featured_slider").owlCarousel({
     loop: true,
     margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplaySpeed: 1000,
+    smartSpeed: 1000,
     nav: true,
     responsive: {
       0: {
@@ -53,17 +57,20 @@ $(document).ready(function () {
   // ct_recent_work_slider Brand Slider S
   $(".ct_testimonial_slider").owlCarousel({
     loop: false,
-    margin: 0,
+    margin: 15,
     nav: true,
     responsive: {
       0: {
         items: 1,
       },
-      600: {
+      575: {
         items: 1,
       },
+      767: {
+        items: 2,
+      },
       1000: {
-        items: 1,
+        items: 3,
       },
     },
   });
@@ -522,3 +529,62 @@ inputRight.addEventListener("mouseup", function () {
 });
 
 // Range Slider js E
+
+var ml4 = {};
+ml4.opacityIn = [0, 1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".ml4 .letters-1",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: ".ml4 .letters-1",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay,
+  })
+  .add({
+    targets: ".ml4 .letters-2",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: ".ml4 .letters-2",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay,
+  })
+  .add({
+    targets: ".ml4 .letters-3",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: ".ml4 .letters-3",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay,
+  })
+  .add({
+    targets: ".ml4",
+    opacity: 0,
+    duration: 500,
+    delay: 500,
+  });
