27ff67476a93366d3e550b250485f5c97219bed9
govindkumawat123 <govind.ctinfotech@gmail.com>
2025-08-14 20:22:45 +0530

    Design changes

--- src/app/components/landing-pages/ecommerce-store-builder/ecommerce-store-builder.component.ts
+++ src/app/components/landing-pages/ecommerce-store-builder/ecommerce-store-builder.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-ecommerce-store-builder',
   standalone: true,
-  imports: [],
+ imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './ecommerce-store-builder.component.html',
   styleUrl: './ecommerce-store-builder.component.css'
 })
