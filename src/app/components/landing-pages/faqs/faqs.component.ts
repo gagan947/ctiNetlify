24601645f68153f3ec117badf9ed00bbb44f67c8
gagan947 <gagan.ctinfotech@gmail.com>
2025-09-15 20:49:30 +0530

    latest change 15/09/2025

--- src/app/components/landing-pages/faqs/faqs.component.ts
+++ src/app/components/landing-pages/faqs/faqs.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-faqs',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './faqs.component.html',
   styleUrl: './faqs.component.css'
 })
