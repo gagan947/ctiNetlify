f33abe7fc655c1d80cae844d2f3b11740b2d2540
faraz78691 <mohdfaraz.github@gmail.com>
2025-10-09 11:49:22 +0530

    changes done in payemnt suces

--- src/app/components/payment-sucessfull/payment-sucessfull.component.ts
+++ src/app/components/payment-sucessfull/payment-sucessfull.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { SidebarComponent } from "../client_buildcard_pages/sidebar/sidebar.component";
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../../services/api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-payment-sucessfull',
@@ -17,6 +18,14 @@ export class PaymentSucessfullComponent {
   clientEnquryId: string = '';
   constructor(private route: ActivatedRoute, private service: ApiService) { }
 
+  // Countdown values
+  days = 0;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+
+  private targetTime: number = 0;
+  private timerSub!: Subscription;
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.orderId = params['order_id'];
@@ -47,5 +56,42 @@ export class PaymentSucessfullComponent {
       this.paymentStatus = 'Error checking status';
     }
   }
+
+    // Steps / status messages
+    steps = [
+      'Initializing project setup',
+      'Design & architecture phase',
+      'Development in progress',
+      'Testing & QA',
+      'Final touches & review',
+      'Preparing for handover'
+    ];
+  
+    // Lottie options if you choose to use a Lottie JSON animation
+    lottieOptions = {
+      path: 'assets/animations/tech_process.json', // your JSON file
+      autoplay: true,
+      loop: true
+    };
+
+    ngOnDestroy() {
+      if (this.timerSub) {
+        this.timerSub.unsubscribe();
+      }
+    }
+  
+    private updateCountdown() {
+      const now = new Date().getTime();
+      let distance = this.targetTime - now;
+  
+      if (distance < 0) {
+        distance = 0;
+      }
+  
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    }
 
 }
