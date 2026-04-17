ae67f1779c1070a10cb58b17a52cae6bd3a8301b
gagan947 <gagan.ctinfotech@gmail.com>
2026-04-17 15:49:30 +0530

    buy more credit page design

---
+++ src/app/components/client_buildcard_pages/buy-more-credits/buy-more-credits.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-more-credits',
+  standalone: true,
+  imports: [],
+  templateUrl: './buy-more-credits.component.html',
+  styleUrl: './buy-more-credits.component.css'
+})
+export class BuyMoreCreditsComponent {
+
+}
