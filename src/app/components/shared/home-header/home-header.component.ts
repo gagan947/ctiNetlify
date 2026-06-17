31ccfc4aaa75c8215eae268af6958c7f8e2628fc
govindkumawat123 <govindkumawat.github@gmail.com>
2026-06-17 11:01:45 +0530

    Design changs

--- src/app/components/shared/home-header/home-header.component.ts
+++ src/app/components/shared/home-header/home-header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HoneFooterComponent } from '../hone-footer/hone-footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-header',
   standalone: true,
-  imports: [HoneFooterComponent],
+  imports: [HoneFooterComponent,RouterLink],
   templateUrl: './home-header.component.html',
   styleUrl: './home-header.component.css'
 })
